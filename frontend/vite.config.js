374f106d4e91daba067f5550b7feb67b27e8190f
d3webdev <32586403+d3webdev@users.noreply.github.com>
2025-01-04 01:08:57 -0300

    Inicial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import process from 'process';
 
-// https://vite.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-//     server: {
-//         port: import.meta.env.VITE_PORT || '3001', // Porta personalizada
-//     },
-// });
-
 export default defineConfig (({ mode }) => {
     const env = loadEnv(mode, process.cwd());
     return {
